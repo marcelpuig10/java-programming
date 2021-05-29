8e39b62d40487064a16644b17adf23505be04b36
marcelpuig10 <marcel_puig@hotmail.com>
2021-05-29 15:47:10 -0400

    composition examples using bus class

---
+++ src/day48_constructors_static/Engine.java
@@ -0,0 +1,20 @@
+package day48_constructors_static;
+
+public class Engine {
+    private int cylinders;
+
+    @Override
+    public String toString() {
+        return "Engine{" +
+                "cylinders=" + cylinders +
+                '}';
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public void setCylinders(int cylinders) {
+        this.cylinders = cylinders;
+    }
+}
