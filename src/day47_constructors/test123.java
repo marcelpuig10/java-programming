115915a7a16c34e85b800ec25e0cd2d329592bac
marcelpuig10 <marcel_puig@hotmail.com>
2021-06-12 10:47:51 -0400

    test

--- src/day47_constructors/test123.java
+++ src/day47_constructors/test123.java
@@ -1,15 +1,20 @@
 package day47_constructors;
 
 public class test123 {
-    public test123(){
-        System.out.println("developer");
-    }
-    public void test123(){
-        System.out.println("sdet");
-    }
+   static int a;
+   int b;
 
     public static void main(String[] args) {
-        test123 obj = new test123();
 
+        test123 obj1 = new test123();
+        obj1.b = 10;
+        obj1.a = 20;
+
+        test123 obj2 = new test123();
+        obj2.b = 30;
+        obj2.a = 40;
+
+        System.out.println(obj1.a + " " + obj1.b);
+        System.out.println(obj2.a + " " + obj2.b);
     }
 }
